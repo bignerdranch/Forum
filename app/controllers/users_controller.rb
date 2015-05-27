04a4e461b3832f2cd7901fd2d85bdc47c64a4a8c
ravencovington <raven@bignerdranch.com>
2015-05-27 17:45:41 -0400

    Added trackable functionality to User schema

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
     @user = current_user
   end
 
+  def sign_in
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      sign_in user
+      redirect_to user_path user
+    else
+      flash[:danger] = 'Invalid email/password combination'
+      render 'new'
+    end
+  end
 end
+
