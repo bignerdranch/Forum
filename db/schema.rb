bda254e58e5672fdb94060c913efbc75b9dc80f3
ravencovington <raven@bignerdranch.com>
2015-06-05 09:39:42 -0400

    Forum Topic model created. Forum Topic must be present and unique.
    [#95331102]

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150529183805) do
+ActiveRecord::Schema.define(version: 20150603172404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "forum_topics", force: :cascade do |t|
+    t.string  "title"
+    t.boolean "archived", default: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
