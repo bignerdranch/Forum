af1b3f458748079d51da5bc2a41d5050d07027a6
ravencovington <raven@bignerdranch.com>
2015-06-05 09:43:59 -0400

    A logged-in User can create a new forum topic with a title.
    [#95331102]

---
+++ spec/features/user_creates_forum_topic_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'feature_helper'
+
+RSpec.feature 'User creates forum topic' do
+
+  before do
+    log_in_with 'raven@bignerdranch.com', 'password'
+  end
+
+  scenario 'user creates a new forum topic' do
+    click_on('Create a new forum topic')
+    fill_in 'Title', with: 'BNR iOS book'
+    click_on('Create Forum topic')
+    expect(page).to have_content("You're at the Forum Topic Page")
+    end
+  end
