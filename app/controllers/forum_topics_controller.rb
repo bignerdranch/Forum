22c06b9ea16265b45b82138fdfbee923b357ed11
ravencovington <raven@bignerdranch.com>
2015-06-05 09:43:59 -0400

    A logged-in User can create a new forum topic with a title.
    [#95331102]

--- app/controllers/forum_topics_controller.rb
+++ app/controllers/forum_topics_controller.rb
@@ -6,11 +6,20 @@ class ForumTopicsController < ApplicationController
 
   def create
     @forum_topic =  ForumTopic.new(forum_topic_params)
+    if @forum_topic.save
+      redirect_to @forum_topic
+    else
+     flash[:notice] = "Oops forum topic wasn't created."
+    end
+  end
+
+  def show
+    @forum_topic = ForumTopic.find(params[:id])
   end
 
   private
 
   def forum_topic_params
-    params.require(:title).permit(:archived)
+    params.require(:forum_topic).permit(:title, :archived)
   end
 end
--- spec/features/user_creates_forum_topic_spec.rb
+++ spec/features/user_creates_forum_topic_spec.rb
@@ -7,14 +7,10 @@ RSpec.feature 'User creates forum topic' do
     log_in_with 'raven@bignerdranch.com', 'password'
   end
 
-  scenario 'User clicks new forum topic' do
+  scenario 'user creates a new forum topic' do
     click_on('Create a new forum topic')
-    expect(page).to have_content('Title:')
-  end
-
-  scenario 'User can create a new forum topic' do
-    visit new_forum_topic_path
     fill_in 'Title', with: 'BNR iOS book'
-    click_on 'Create Forum topic'
+    click_on('Create Forum topic')
+    expect(page).to have_content("You're at the Forum Topic Page")
+    end
   end
-end
