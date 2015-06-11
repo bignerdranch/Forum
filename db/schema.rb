a16e80df704eb47ea8afcd218f485060503faf0f
ravencovington <raven@bignerdranch.com>
2015-06-11 11:33:45 -0400

    Sets null constraint to username column in User table in database
    [#95331070]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610142602) do
+ActiveRecord::Schema.define(version: 20150611143524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 20150610142602) do
     t.string   "twitter_account"
     t.text     "bio"
     t.string   "position"
-    t.string   "username"
+    t.string   "username",                            null: false
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
