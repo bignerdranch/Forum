1236447e2318e4261bfb22fc682124a2e867d8bc
ravencovington <raven@bignerdranch.com>
2015-06-12 13:03:54 -0400

    Adds username attribute to feature test
    [#95331102]

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -1,7 +1,7 @@
 def log_in_with(email, password)
-  User.create!(email:email, password: password, confirmed_at: 1.minute.ago)
+  User.create!(username:'raven', email:email, password: password, confirmed_at: 1.minute.ago)
   visit new_user_session_path
-  fill_in 'Email', with: email
+  fill_in 'Login', with: email
   fill_in 'user_password', with: password
   click_button('Log in')
 end
