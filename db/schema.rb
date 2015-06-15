ef4ba9cf91faaf4d41d74527ca598d1670baca4d
ravencovington <raven@bignerdranch.com>
2015-06-15 15:22:00 -0400

    Adds User Id to Forum Topic table
    [#95331106]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615144603) do
+ActiveRecord::Schema.define(version: 20150615191912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150615144603) do
   create_table "forum_topics", force: :cascade do |t|
     t.string  "title"
     t.boolean "archived", default: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
