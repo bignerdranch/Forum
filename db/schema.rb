969c06ddeb72214e36c28f2447f3d3f2fb5faa30
ravencovington <raven@bignerdranch.com>
2015-06-15 15:31:57 -0400

    Adds User Id to Posts table
    [#95331106]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150615191912) do
+ActiveRecord::Schema.define(version: 20150615192322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20150615191912) do
     t.integer  "forum_topic_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
