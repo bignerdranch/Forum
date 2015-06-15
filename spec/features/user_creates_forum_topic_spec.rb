0c5cd2b1ba199c69d65e047491d230ad36a03314
ravencovington <raven@bignerdranch.com>
2015-06-15 10:17:25 -0400

    User can view forum topic index at home page and navigate back to it
    [#95331106]

--- spec/features/user_creates_forum_topic_spec.rb
+++ spec/features/user_creates_forum_topic_spec.rb
@@ -7,18 +7,18 @@ RSpec.feature "User creates forum topic" do
     log_in_with "raven@bignerdranch.com", "password"
   end
 
-  scenario "user creates a new forum topic" do
-    click_on("Create a new forum topic")
-    fill_in "Title", with: "BNR iOS book"
-    click_on("Create Forum topic")
-    expect(page).to have_content("Forum Topic was successfully created.")
-    expect(page).to have_selector(:link_or_button, "Archive")
+  scenario 'user creates a new forum topic' do
+    click_on('Create New Forum Topic')
+    fill_in 'Title', with: 'BNR iOS book'
+    click_on('Create Forum topic')
+    expect(page).to have_content("You're at the Forum Topic Page")
+    expect(page).to have_selector(:link_or_button, 'Archive')
   end
 
-  scenario "user updates state of forum" do
-    click_on("Create a new forum topic")
-    fill_in "Title", with: "BNR iOS book"
-    click_on("Create Forum topic")
+  scenario 'user updates state of forum' do
+    click_on('Create New Forum Topic')
+    fill_in 'Title', with: 'BNR iOS book'
+    click_on('Create Forum topic')
     expect(page).to have_content("You're at the Forum Topic Page")
     expect(page).to have_selector(:link_or_button, "Archive")
     click_on("Archive")
