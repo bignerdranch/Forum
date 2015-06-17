62ae040411335d1c12c7e3dc4d9254f393a3f962
ravencovington <raven@bignerdranch.com>
2015-06-17 17:22:03 -0400

    Adds avatar to user table
    [#96029340]

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616230000) do
+ActiveRecord::Schema.define(version: 20150617211145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,15 +19,6 @@ ActiveRecord::Schema.define(version: 20150616230000) do
   create_table "forum_topics", force: :cascade do |t|
     t.string  "title"
     t.boolean "archived", default: false
-    t.integer "user_id"
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.text     "content"
-    t.integer  "forum_topic_id", null: false
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -51,12 +42,11 @@ ActiveRecord::Schema.define(version: 20150616230000) do
     t.string   "twitter_account"
     t.text     "bio"
     t.string   "position"
-    t.string   "username",                            null: false
+    t.string   "avatar"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
-  add_index "users", ["username"], name: "index_users_on_username", unique: true, using: :btree
 
 end
