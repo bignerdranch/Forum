6033ab902d88536e662660c628c8cc0744399c32
ravencovington <raven@bignerdranch.com>
2015-06-24 06:45:42 -0400

    * Adds feature test scenarios for identical and blank titles * Adds feature helper method to clean up test
    [#95331102]

--- spec/features/user_creates_forum_topic_spec.rb
+++ spec/features/user_creates_forum_topic_spec.rb
@@ -5,20 +5,15 @@ RSpec.feature "User creates forum topic" do
 
   before do
     log_in_with "raven@bignerdranch.com", "password"
+    create_new_forum_topic "BNR iOS book"
   end
 
   scenario 'user creates a new forum topic' do
-    click_on('Create New Forum Topic')
-    fill_in 'Title', with: 'BNR iOS book'
-    click_on('Create Forum topic')
     expect(page).to have_content("You're at the Forum Topic Page")
     expect(page).to have_selector(:link_or_button, 'Archive')
   end
 
   scenario 'user updates state of forum' do
-    click_on('Create New Forum Topic')
-    fill_in 'Title', with: 'BNR iOS book'
-    click_on('Create Forum topic')
     expect(page).to have_content("You're at the Forum Topic Page")
     expect(page).to have_selector(:link_or_button, "Archive")
     click_on("Archive")
@@ -30,5 +25,13 @@ RSpec.feature "User creates forum topic" do
     fill_in 'Title', with: ''
     click_on('Create Forum topic')
     expect(page).to have_content("Title can't be blank")
+  end
+
+  scenario 'user enters identical forum topic title' do
+    click_on('Create New Forum Topic')
+    fill_in 'Title', with: 'BNR iOS book'
+    click_on('Create Forum topic')
+    expect(page).to have_content('Title has already been taken')
+    expect(page.current_path).to eq(forum_topics_path)
   end
 end
