35be3622509f3df7628a714a7da6aad7113fa98a
ravencovington <raven@bignerdranch.com>
2015-07-22 13:39:41 -0400

    Configures AWS S3
    * Removes default url method
    * Stores files in fog(cloud)
    * Changes avatar_url to avatar to meet S3 convention
    
    [#98665424]

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,11 +1,7 @@
 class AvatarUploader < CarrierWave::Uploader::Base
-  storage :file
+  storage :fog
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-
-  def default_url
-    ActionController::Base.helpers.asset_path("fallback/" + [version_name, "default-avatar.png"].compact.join('_'))
   end
 end
